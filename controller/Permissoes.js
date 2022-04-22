106027a5bc6945ce512d19968f8309c618262487
maiksosn <michael4035@edu.faccar.com.br>
2022-04-22 09:46:05 -0300

    Feito em casa 01

--- controller/Permissoes.js
+++ controller/Permissoes.js
@@ -1,18 +1,33 @@
 const Permissoes = require('../model/Especialidade')
-const { store, update } = require('./User')
 
 module.exports = {
+    
+    // LER  -> READ
     async index(req, res){
         let permissoes = await Permissoes.find()
         return res.json(permissoes)
     },
-
+    
+    // CRIAR -> CREATE
     async store(req, res){
         let permissoes = req.body
         permissoes = await permissoes.create(permissoes)
         return res.json(permissoes)
     },
 
+    // MODIFICAR -> UPDATE
+    async update(req, res){
+        let permissoes = req.body
+        permissoes = await Permissoes.updateOne({'_id': req.query.id},Permissoes)
+        return res.json(permissoes)
+    },
+
+    // DELETAR -> DELETE
+    async delete(req, res){
+        let Permissoes = req.body
+        permissoes = await Permissoes.deleteOne({'_id': req.query.id},permissoes)
+        return res.json(permissoes)
+    }
 
        
 }
